8d689de4d9cda07757141d1a3790d02aac33e525
hungtqclone <121530023+hungtqclone@users.noreply.github.com>
2023-08-06 00:27:58 +0700

    hungtq Update scaner qrcode

--- App.js
+++ App.js
@@ -7,18 +7,23 @@ import Test from './src/screens/Test';
 import Testloing from './src/screens/Test'
 import TesTMap from './src/test/ChatTest'
 import VideoCall from './src/screens/VideoCall'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import ScanQrCode from './src/screens/ScanQrCode';
 
 
 const App = () => {
 
   return (
-    <AppContextProvider>
-      <NavigationContainer>
-        {/* <VideoCall /> */}
-        <BottomTabs/>
-        {/* <TesTMap /> */}
-      </NavigationContainer>
-    </AppContextProvider>
+    // <AppContextProvider>
+    //   <NavigationContainer>
+    //     {/* <VideoCall /> */}
+    //     <BottomTabs/>
+    //     {/* <TesTMap /> */}
+    //   </NavigationContainer>
+    // </AppContextProvider>
+    <SafeAreaView style={{flex:1}}>
+        <ScanQrCode/>
+    </SafeAreaView>
   )
 }
 export default App
