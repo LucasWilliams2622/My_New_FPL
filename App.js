c41535dbff6dba43f436dbbcf0cde5121e4786aa
trung-hieu-vscode <94751659+trung-hieu-vscode@users.noreply.github.com>
2023-09-07 16:30:03 +0700

    upbottontabgofpt

--- App.js
+++ App.js
@@ -48,6 +48,7 @@ const App = () => {
       <NavigationContainer>
         
         {isLoading ? <Splash setIsLoading={setIsLoading} /> : <BottomTabs />}
+       
       
        
         {/* <TestShare /> */}
