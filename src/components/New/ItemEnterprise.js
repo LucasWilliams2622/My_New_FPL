35fcdf3880be22ca86c9836b13b3ff937df7a758
hungtqclone <121530023+hungtqclone@users.noreply.github.com>
2023-09-25 20:10:35 +0700

    hungtq update design New

--- src/components/New/ItemEnterprise.js
+++ src/components/New/ItemEnterprise.js
@@ -15,18 +15,20 @@ const ItemEnterprise = (props) => {
         
     }
     return (
-        <TouchableOpacity onPress={()=> goDetail()}  style={[AppStyle.item, { width: 360, marginVertical: 5, }]}>
-                <Image style={{ width:"100%", height: 140, borderRadius: 8 }} source={{uri: data.image}} />
+        <TouchableOpacity onPress={() => goDetail()} style={[AppStyle.item, { width: 360, paddingHorizontal: 0, paddingVertical: 0, borderRadius: 30,marginBottom:20 }]}>
+            <Image style={{ width: "100%", height: 140, borderTopLeftRadius: 30, borderTopRightRadius: 30 }} source={{uri: data.image}} />
 
-            <Text style={[AppStyle.titleBig, {textAlign:"center",marginTop:10 }]} numberOfLines={1}>{data.title}</Text>
-          
-                    <Text style={[AppStyle.titleSmall, { marginTop:10,width:'80%',color:COLOR.text,fontWeight:'400' }]}  numberOfLines={1}>{data.content}</Text>
-           
-            
-            <View style={[AppStyle.row, { marginTop: 15 }]}>
-                <Text style={[AppStyle.text, { flex: 1 }]}>Người Đăng :{data.author}</Text>
-             
-                <Text style={[AppStyle.text, { flex: 1 ,textAlign:'right'}]}>Thời gian :{data.date.slice(0,10)}</Text>
+            <View style={{paddingHorizontal:10,marginBottom: 12}}>
+                <Text style={[AppStyle.titleBig, { textAlign: "center", marginTop: 10 }]} numberOfLines={1}>{data.title}</Text>
+
+                <Text style={[AppStyle.titleSmall, { marginTop: 10, width: '80%', color: COLOR.text, fontWeight: '400' }]} numberOfLines={1}>{data.content}</Text>
+
+
+                <View style={[AppStyle.row, { marginTop: 15 }]}>
+                    <Text style={[AppStyle.text, { flex: 1 }]}>Người Đăng :{data.author}</Text>
+
+                    <Text style={[AppStyle.text, { flex: 1, textAlign: 'right' }]}>Thời gian :{data.date.slice(0, 10)}</Text>
+                </View>
             </View>
         </TouchableOpacity>
     )
