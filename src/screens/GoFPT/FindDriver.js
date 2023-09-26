6369d43ef3f9345f6fdf545d3218ed1454e417e9
DThanh0 <115547764+DThanh0@users.noreply.github.com>
2023-09-27 03:36:32 +0700

    sorrt_V2

--- src/screens/GoFPT/FindDriver.js
+++ src/screens/GoFPT/FindDriver.js
@@ -10,6 +10,53 @@ import ItemFindDriver from '../../components/GoFPT/ItemFindDiver'
 import { MotiView, MotiText } from 'moti'
 import TimerMixin from 'react-timer-mixin';
 import Toast from 'react-native-toast-message';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+const sampleData = [
+  {
+    id: '1',
+    title: 'Driver 1',
+    price: 50,
+    date: '2023-09-27',
+    time: '12:00 AM',
+  },
+  {
+    id: '2',
+    title: 'Driver 2',
+    price: 40,
+    date: '2023-09-28',
+    time: '10:00 AM',
+  },
+  {
+    id: '3',
+    title: 'Driver 3',
+    price: 60,
+    date: '2023-09-29',
+    time: '11:00 AM',
+  },
+  {
+    id: '4',
+    title: 'Driver 14',
+    price: 100000000,
+    date: '2023-09-27',
+    time: '09:00 AM',
+  },
+  {
+    id: '5',
+    title: 'Driver 12',
+    price: 400000000,
+    date: '2023-09-12',
+    time: '05:00 AM',
+  },
+  {
+    id: '6',
+    title: 'Driver 13',
+    price: 60000000,
+    date: '2023-09-26',
+    time: '01:00 AM',
+  },
+  // Thêm các mục khác tại đây
+];
 
 const FindDriver = () => {
   const [dataFindDriver, setDataFindDriver] = useState([])
@@ -23,82 +70,76 @@ const FindDriver = () => {
   const [originalData, setOriginalData] = useState([]); // Dữ liệu gốc
   const [sortBy, setSortBy] = useState(null);
 
+  const [priceRangeStart, setPriceRangeStart] = useState('');
+  const [priceRangeEnd, setPriceRangeEnd] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
+
+  const filterDataByPriceRange = (data, a, b) => {
+    return data.filter(item => item.price >= a && item.price <= b);
+  };
+  const handleFilterByPrice = () => {
+    const filteredData = filterDataByPriceRange(dataFindDriver, parseFloat(priceRangeStart), parseFloat(priceRangeEnd));
+    setDataFindDriver(filteredData);
+    console.log('====================================');
+    console.log(filteredData);
+    console.log('====================================');
+    toggleModal()
+  }
+
+  const handleSortByDate = () => {
+    const sortedData = dataFindDriver.filter((item) => {
+      const itemDate = item.dateStart.slice(0, 10);
+      return itemDate >= startDate && itemDate <= endDate;
+    });
+
+    // Sắp xếp dữ liệu theo ngày
+    sortedData.sort((a, b) => {
+      const dateA = new Date(a.dateStart.slice(0, 10));
+      const dateB = new Date(b.dateStart.slice(0, 10));
+      return dateA - dateB;
+    });
+
+    setDataFindDriver(sortedData);
+    console.log('====================================');
+    console.log(sortedData);
+    console.log('====================================');
+    toggleModal()
+  };
+
+  const handleSortByTime = () => {
+    const sortedData = dataFindDriver.filter((item) => {
+      const itemTime = item.timeStart?.slice(12, 16);
+      const startTimeObj = startTime;
+      const endTimeObj = endTime;
+
+      return itemTime >= startTimeObj && itemTime <= endTimeObj;
+    });
+
+    // Sắp xếp dữ liệu theo giờ
+    sortedData.sort((a, b) => {
+      const timeA = a.timeStart?.slice(12, 16);
+      const timeB = b.timeStart?.slice(12, 16);
+      return timeA.localeCompare(timeB);
+
+    });
+
+    setDataFindDriver(sortedData);
+    toggleModal()
+    console.log('====================================');
+    console.log(sortedData);
+    console.log('====================================');
+  };
 
   useEffect(() => {
-    // const sampleData = [
-    //   {
-    //     id: '1',
-    //     title: 'Driver 1',
-    //     price: 50,
-    //     date: '2023-09-27',
-    //     time: '12:00 AM',
-    //   },
-    //   {
-    //     id: '2',
-    //     title: 'Driver 2',
-    //     price: 40,
-    //     date: '2023-09-28',
-    //     time: '10:00 AM',
-    //   },
-    //   {
-    //     id: '3',
-    //     title: 'Driver 3',
-    //     price: 60,
-    //     date: '2023-09-29',
-    //     time: '11:00 AM',
-    //   },
-    //   {
-    //     id: '4',
-    //     title: 'Driver 14',
-    //     price: 100000000,
-    //     date: '2023-09-27',
-    //     time: '09:00 AM',
-    //   },
-    //   {
-    //     id: '5',
-    //     title: 'Driver 12',
-    //     price: 400000000,
-    //     date: '2023-09-12',
-    //     time: '05:00 AM',
-    //   },
-    //   {
-    //     id: '6',
-    //     title: 'Driver 13',
-    //     price: 60000000,
-    //     date: '2023-09-26',
-    //     time: '01:00 AM',
-    //   },
-    //   // Thêm các mục khác tại đây
-    // ];
     setDataFindDriver(dataFindDriver);
     setOriginalData(dataFindDriver); // Lưu trữ dữ liệu gốc
   }, []);
 
-
-
   const toggleModal = async () => {
     setModalVisible(!ModalVisible);
-  };
-
-  const handleSortBy = (criteria) => {
-    const sortedData = dataFindDriver.sort((a, b) => {
-      setSortBy(criteria);
-      toggleModal();
-
-      // Sắp xếp danh sách dựa trên tiêu chí
-      if (criteria === 'price') {
-        // Sắp xếp theo giá (ví dụ)
-        setDataFindDriver([...dataFindDriver].sort((b, a) => a.price - b.price));
-      } else if (criteria === 'date') {
-        // Sắp xếp theo ngày (ví dụ)
-        setDataFindDriver([...dataFindDriver].sort((b, a) => new Date(a.dateStart.slice(0,10)) - new Date(b.dateStart.slice(0,10))));
-      } else if (criteria === 'time') {
-        // Sắp xếp theo giờ (ví dụ)
-        setDataFindDriver([...dataFindDriver].sort((b, a) => Date(a.timeStart?.slice(12,16)) - Date(b.timeStart?.slice(12,16))));
-      }
-
-    });
-
   };
 
   const resetSort = () => {
@@ -106,8 +147,20 @@ const FindDriver = () => {
     setDataFindDriver(originalData); // Khôi phục danh sách gốc
     toggleModal();
   };
-  ;
 
+  const handleApplySort = () => {
+    if (sortType === 'date') {
+      // Sắp xếp theo ngày ở đây
+      handleSortByDate();
+    } else if (sortType === 'time') {
+      // Sắp xếp theo giờ ở đây
+      handleSortByTime();
+    } else if (sortType === 'price') {
+      // Sắp xếp theo giá ở đây
+      filterDataByPriceRange();
+    }
+    toggleModal(); // Đóng modal sau khi áp dụng sắp xếp
+  };
 
   const [keyword, setKeyword] = useState('')
   useEffect(() => {
@@ -208,37 +261,80 @@ const FindDriver = () => {
         transparent={true}
         visible={ModalVisible}
         onRequestClose={() => setModalVisible(false)}>
-        <View style={AppStyle.modalBackground}>
-          <View style={[AppStyle.modalView, { height: 540 }]}>
-            <View style={AppStyle.viewheadModal}>
-              <TouchableOpacity
-                style={AppStyle.btnX}
-                onPress={() => setModalVisible(false)}>
-                <Image
-                  source={require('../../assets/icons/ic_close.png')}
-                />
+        <KeyboardAwareScrollView
+          style={{}}
+          contentContainerStyle={{}}
+          enableOnAndroid={true}
+          enableAutomaticScroll={true}
+        >
+          <View style={AppStyle.modalBackground}>
+            <View style={[AppStyle.modalView, { height: 540 }]}>
+              <View style={AppStyle.viewheadModal}>
+                <TouchableOpacity
+                  style={AppStyle.btnX}
+                  onPress={() => setModalVisible(false)}>
+                  <Image
+                    source={require('../../assets/icons/ic_close.png')}
+                  />
+                </TouchableOpacity>
+                <Text style={AppStyle.txtModal1}>Tìm bạn cho chuyến đi</Text>
+              </View>
+              <Text>Nhập khoảng giá:</Text>
+              <TextInput
+                placeholder="Giá bắt đầu (a)"
+                keyboardType="numeric"
+                value={priceRangeStart}
+                onChangeText={(text) => setPriceRangeStart(text)}
+              />
+              <TextInput
+                placeholder="Giá kết thúc (b)"
+                keyboardType="numeric"
+                value={priceRangeEnd}
+                onChangeText={(text) => setPriceRangeEnd(text)}
+              />
+              <TouchableOpacity onPress={() => handleFilterByPrice()}>
+                <Text>Lọc theo giá</Text>
               </TouchableOpacity>
-              <Text style={AppStyle.txtModal1}>Tìm bạn cho chuyến đi</Text>
-            </View>
-            <TouchableOpacity onPress={() => handleSortBy('price')}>
-              <Text>Sắp xếp theo giá</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleSortBy('date')}>
-              <Text>Sắp xếp theo ngày</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleSortBy('time')}>
-              <Text>Sắp xếp theo giờ</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={resetSort}>
-              <Text>Reset</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={toggleModal}>
-              <Text>Áp dụng sắp xếp</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
 
+              <Text>Nhập khoảng ngày:</Text>
+              <TextInput
+                placeholder="Ngày bắt đầu (yyyy-MM-dd)"
+                value={startDate}
+                onChangeText={(text) => setStartDate(text)}
+              />
+              <TextInput
+                placeholder="Ngày kết thúc (yyyy-MM-dd)"
+                value={endDate}
+                onChangeText={(text) => setEndDate(text)}
+              />
+              <TouchableOpacity onPress={() => handleSortByDate()}>
+                <Text>Sắp xếp theo ngày</Text>
+              </TouchableOpacity>
+
+              <Text>Nhập khoảng giờ (24 giờ):</Text>
+              <TextInput
+                placeholder="Giờ bắt đầu (hh:mm)"
+                value={startTime}
+                onChangeText={(text) => setStartTime(text)}
+              />
+              <TextInput
+                placeholder="Giờ kết thúc (hh:mm)"
+                value={endTime}
+                onChangeText={(text) => setEndTime(text)}
+              />
+              <TouchableOpacity onPress={() => handleSortByTime()}>
+                <Text>Sắp xếp theo giờ</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={handleApplySort}>
+                <Text>Apply</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={resetSort}>
+                <Text>Reset</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </KeyboardAwareScrollView>
+      </Modal>
     </MotiView>
   )
 }
